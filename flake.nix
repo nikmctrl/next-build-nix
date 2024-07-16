d5279a782315583590c9d71d01d3ab59f56b5780
Nikolai Schimke <nikolai@missionctrl.dev>
2024-07-17 00:15:03 +0100

    updating flake

--- flake.nix
+++ flake.nix
@@ -42,7 +42,13 @@
         default = final: prev: {
           # Example package
           next-app = final.napalm.buildPackage ./. { 
-            __noChroot = true;
+            buildPhase = ''
+              npm run postinstall
+
+              mkdir -p $out/bin
+              cp -r ./dist $out/bin/next-app
+              chmod +x $out/bin/next-app
+            '';
           };
         };
       };
@@ -54,29 +60,7 @@
         # The default package for 'nix build'. This makes sense if the
         # flake provides only one package or there is a clear "main"
         # package.
-        default = pkgs.stdenv.mkDerivation {
-          name = "next-build-nix";
-          src = ./.;
-          buildInputs = [ pkgs.next-app pkgs.nodejs ];
-
-          __noChroot = true;
-
-          buildPhase = ''
-            cp -r ${pkgs.next-app}/_napalm-install/ $out
-            cd $out
-            mkdir .next
-            ${pkgs.nodejs}/bin/npm run build
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp -r .next/standalone $out/bin/next-build-nix-app
-
-            echo "#!${pkgs.nodejs}/bin/node" > $out/bin/next-build-nix
-            echo "require('$out/bin/next-build-nix-app/server/server.js')" >> $out/bin/next-build-nix
-            chmod +x $out/bin/next-build-nix
-          '';
-        };
+        default = self.packages.${system}.next-app;
       });
     };
 }
